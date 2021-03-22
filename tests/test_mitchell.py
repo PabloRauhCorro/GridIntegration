f7ee3c32dfa56b3255fcc84a9bdd91b83c5ad325
PabloRauhCorro <pablorauhcorro@yahoo.de>
2021-03-22 13:24:27 +0100

    bug found

--- tests/test_mitchell.py
+++ tests/test_mitchell.py
@@ -4,38 +4,39 @@ from scipy.integrate import tplquad
 sys.path.append(os.getcwd())
 from Mitchell import Mitchell_Integration
 
-# test function
-def test_integrand0(alpha, beta, gamma):
-    return np.sin(alpha) * np.sin(beta) * np.sin(gamma)
+# test integrands
 
 def test_integrand1(alpha, beta, gamma):
     return np.sin(beta) * np.cos(beta)**2
+def test_integrand2(alpha, beta, gamma):
+    return np.sin(alpha) * np.sin(beta) * np.sin(gamma)
 
-
-# Calculate integral using Mitchell's simple grid
-mitchell_integration = Mitchell_Integration()
-TI0_mitchell = mitchell_integration.integrate_function(test_integrand0, 1000)
-TI1_mitchell = mitchell_integration.integrate_function(test_integrand1, 1000)
-print("Mitchell simple grid: ")
-print("Test Integrand 0: " +   str(TI0_mitchell))
-print("Test Integrand 1: " + str(TI1_mitchell))
-print("\n")
-
-#Compare to the results obtained with tplquad
+# Integration with tplquad
 alpha_range = [0.0, 2*np.pi] 
 beta_range = [0.0, np.pi]
 gamma_range = [0.0, 2*np.pi]
-TI0 = tplquad(test_integrand0, gamma_range[0], gamma_range[1], lambda gamma: beta_range[0], lambda gamma: beta_range[1], 
-            lambda gamma, beta: alpha_range[0], lambda gamma, beta: alpha_range[1] )[0] 
-TI1 = tplquad(test_integrand1, 
+ 
+TI1 = tplquad(test_integrand1, gamma_range[0], gamma_range[1], lambda gamma: beta_range[0], lambda gamma: beta_range[1], 
+            lambda gamma, beta: alpha_range[0], lambda gamma, beta: alpha_range[1] )[0]             
+TI2 = tplquad(test_integrand2, 
 				  gamma_range[0], gamma_range[1], 
 				  lambda gamma: beta_range[0], lambda gamma: beta_range[1], 
 				  lambda gamma, beta: alpha_range[0], lambda gamma, beta: alpha_range[1])[0]
+
 print("Scipy TplQuad: ")
-print("Test Integrand 0: " +  str(TI0))
 print("Test Integrand 1: " +  str(TI1))
+print("Test Integrand 2: " +  str(TI2))
 print("\n")
 
-# calculate the deviations
-print("Deviation of Test Integrand 0:" + str(100*(TI0-TI0_mitchell)/TI0) + "%")
-print("Deviation of Test Integrand 1:" + str(100*(TI1-TI1_mitchell)/TI1) + "%")
+# Calculate integral using Mitchell's simple grid
+mitchell_integration = Mitchell_Integration()
+
+TI1_mitchell = mitchell_integration.integrate_function(test_integrand1, 1000)
+TI2_mitchell = mitchell_integration.integrate_function(test_integrand2, 1000)
+print("Integration using Mitchell simple grid: ")
+
+print("Test Integrand 1: " + str(TI1_mitchell) + " - Deviation: " + str(100*(TI1-TI1_mitchell)/TI1) + "%")
+print("Test Integrand 2: " + str(TI2_mitchell) + " - Deviation: " + str(100*(TI2-TI2_mitchell)/TI2) + "%")
+print("\n")
+
+
