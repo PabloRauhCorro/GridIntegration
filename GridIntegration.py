667eafba3e1e5df4c62541999d31a724127a5459
PabloRauhCorro <pablorauhcorro@yahoo.de>
2021-03-29 13:12:30 +0200

    refactor

--- GridIntegration.py
+++ GridIntegration.py
@@ -16,10 +16,4 @@ class GridIntegration(ABC):
     def integrate_function(self, function):
         pass
 
-    
-    """ def integrate_function(self, function, dimension, integration_ranges):
-        if integration_ranges is not None and dimension != len(integration_ranges):
-            raise ValueError("Integration ranges don't match number of dimensions.")
-        weights = self.get_weights(dimension, integration_ranges)
-        points = self.get_points(dimension, integration_ranges)
-        return np.sum(function(points) * weights) """
+ 
